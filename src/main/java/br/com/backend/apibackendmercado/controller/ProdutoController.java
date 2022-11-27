989fb2ae54c31bd8854d438608d70feb0a57aac4
Hilberto22 <betogouveia1@outlook.com>
2022-11-27 19:06:03 -0300

    criacao dos metodos da classe ProdutoController

---
+++ src/main/java/br/com/backend/apibackendmercado/controller/ProdutoController.java
@@ -0,0 +1,80 @@
+package br.com.backend.apibackendmercado.controller;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.backend.apibackendmercado.model.Produto;
+import br.com.backend.apibackendmercado.services.ProdutoService;
+
+@RestController()
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+    
+    @Autowired
+    private ProdutoService produtoService;
+
+    /**
+     * 
+     * @return
+     */
+    @GetMapping("/listarTodos")
+    public List<Produto> listarTodos(){
+        return produtoService.listarTodos();
+    }
+
+    /**
+     * 
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Produto>> listarPorId(@PathVariable UUID id){
+       return new ResponseEntity<>(produtoService.listarPorId(id), HttpStatus.OK);
+    } 
+
+    /**
+     * 
+     * @param produto
+     * @return
+     */
+    @PostMapping("/cadastrarProduto")
+    public Produto cadastrarProduto(@RequestBody Produto produto){
+        return produtoService.cadastrarProduto(produto);
+    }
+
+    /**
+     * 
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable UUID id){
+        produtoService.deletarProduto(id);
+        return "Produto com id:" + id + "foi deletado com sucesso";
+    }
+    
+    /**
+     * 
+     * @param produto
+     * @param id
+     * @return
+     */
+    @PutMapping("/atualizar")
+    public Produto updateProduto(@RequestBody Produto produto, UUID id){
+        return produtoService.updateProduto(id, produto);
+    }
+}
+ 
