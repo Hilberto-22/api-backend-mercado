457eedabf04cd56360d309efcdbe21086acb4d48
Hilberto22 <betogouveia1@outlook.com>
2023-05-08 01:07:37 -0300

    Atualização da versão do spring e remocao da anotação @Repository

--- src/main/java/br/com/backend/apibackendmercado/repository/ProdutoRepository.java
+++ src/main/java/br/com/backend/apibackendmercado/repository/ProdutoRepository.java
@@ -3,11 +3,8 @@ package br.com.backend.apibackendmercado.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import br.com.backend.apibackendmercado.model.Produto;
 
-@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, UUID> {
     
 }
